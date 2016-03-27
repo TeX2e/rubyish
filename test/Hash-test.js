4a6105a56ed5408bc5c68934bfa7b33de9093c9a
Mako <mnfeconicu41@gmail.com>
2016-03-27 20:50:28 +0900

    miscellaneous

---
+++ test/Hash-test.js
@@ -0,0 +1,442 @@
+
+var assert = require('assert');
+require('../lib/Hash');
+
+describe('Hash', () => {
+  describe('#all?', () => {
+    //
+  });
+
+  describe('#any?', () => {
+    //
+  });
+
+  describe('#assoc', () => {
+    //
+  });
+
+  describe('#chunk', () => {
+    //
+  });
+
+  describe('#chunk_while', () => {
+    //
+  });
+
+  describe('#clear', () => {
+    //
+  });
+
+  describe('#clone', () => {
+    //
+  });
+
+  describe('#collect', () => {
+    //
+  });
+
+  describe('#collect_concat', () => {
+    //
+  });
+
+  describe('#count', () => {
+    //
+  });
+
+  describe('#cycle', () => {
+    //
+  });
+
+  describe('#default', () => {
+    //
+  });
+
+  describe('#default=', () => {
+    //
+  });
+
+  describe('#default_proc', () => {
+    //
+  });
+
+  describe('#default_proc=', () => {
+    //
+  });
+
+  describe('#delete', () => {
+    //
+  });
+
+  describe('#delete_if', () => {
+    //
+  });
+
+  describe('#detect', () => {
+    //
+  });
+
+  describe('#dig', () => {
+    //
+  });
+
+  describe('#display', () => {
+    //
+  });
+
+  describe('#drop', () => {
+    //
+  });
+
+  describe('#drop_while', () => {
+    //
+  });
+
+  describe('#dup', () => {
+    //
+  });
+
+  describe('#each', () => {
+    //
+  });
+
+  describe('#each_cons', () => {
+    //
+  });
+
+  describe('#each_entry', () => {
+    //
+  });
+
+  describe('#each_key', () => {
+    //
+  });
+
+  describe('#each_pair', () => {
+    //
+  });
+
+  describe('#each_slice', () => {
+    //
+  });
+
+  describe('#each_value', () => {
+    //
+  });
+
+  describe('#each_with_index', () => {
+    //
+  });
+
+  describe('#each_with_object', () => {
+    //
+  });
+
+  describe('#empty?', () => {
+    //
+  });
+
+  describe('#entries', () => {
+    //
+  });
+
+  describe('#eql?', () => {
+    //
+  });
+
+  describe('#equal?', () => {
+    //
+  });
+
+  describe('#extend', () => {
+    //
+  });
+
+  describe('#fetch', () => {
+    //
+  });
+
+  describe('#fetch_values', () => {
+    //
+  });
+
+  describe('#find', () => {
+    //
+  });
+
+  describe('#find_all', () => {
+    //
+  });
+
+  describe('#find_index', () => {
+    //
+  });
+
+  describe('#first', () => {
+    //
+  });
+
+  describe('#flat_map', () => {
+    //
+  });
+
+  describe('#flatten', () => {
+    //
+  });
+
+  describe('#grep', () => {
+    //
+  });
+
+  describe('#grep_v', () => {
+    //
+  });
+
+  describe('#group_by', () => {
+    //
+  });
+
+  describe('#has_key?', () => {
+    //
+  });
+
+  describe('#has_value?', () => {
+    //
+  });
+
+  describe('#hash', () => {
+    //
+  });
+
+  describe('#include?', () => {
+    //
+  });
+
+  describe('#index', () => {
+    //
+  });
+
+  describe('#inject', () => {
+    //
+  });
+
+  describe('#inspect', () => {
+    //
+  });
+
+  describe('#invert', () => {
+    //
+  });
+
+  describe('#keep_if', () => {
+    //
+  });
+
+  describe('#key', () => {
+    //
+  });
+
+  describe('#key?', () => {
+    //
+  });
+
+  describe('#keys', () => {
+    //
+  });
+
+  describe('#lazy', () => {
+    //
+  });
+
+  describe('#length', () => {
+    //
+  });
+
+  describe('#map', () => {
+    //
+  });
+
+  describe('#max', () => {
+    //
+  });
+
+  describe('#max_by', () => {
+    //
+  });
+
+  describe('#member?', () => {
+    //
+  });
+
+  describe('#merge', () => {
+    //
+  });
+
+  describe('#min', () => {
+    //
+  });
+
+  describe('#min_by', () => {
+    //
+  });
+
+  describe('#minmax', () => {
+    //
+  });
+
+  describe('#minmax_by', () => {
+    //
+  });
+
+  describe('#nil?', () => {
+    //
+  });
+
+  describe('#none?', () => {
+    //
+  });
+
+  describe('#one?', () => {
+    //
+  });
+
+  describe('#partition', () => {
+    //
+  });
+
+  describe('#rassoc', () => {
+    //
+  });
+
+  describe('#reduce', () => {
+    //
+  });
+
+  describe('#rehash', () => {
+    //
+  });
+
+  describe('#reject', () => {
+    //
+  });
+
+  describe('#replace', () => {
+    //
+  });
+
+  describe('#reverse_each', () => {
+    //
+  });
+
+  describe('#select', () => {
+    //
+  });
+
+  describe('#select!', () => {
+    //
+  });
+
+  describe('#shift', () => {
+    //
+  });
+
+  describe('#size', () => {
+    //
+  });
+
+  describe('#slice_after', () => {
+    //
+  });
+
+  describe('#slice_before', () => {
+    //
+  });
+
+  describe('#slice_when', () => {
+    //
+  });
+
+  describe('#sort', () => {
+    //
+  });
+
+  describe('#sort_by', () => {
+    //
+  });
+
+  describe('#store', () => {
+    //
+  });
+
+  describe('#take', () => {
+    //
+  });
+
+  describe('#take_while', () => {
+    //
+  });
+
+  describe('#tap', () => {
+    //
+  });
+
+  describe('#to_a', () => {
+    //
+  });
+
+  describe('#to_enum', () => {
+    //
+  });
+
+  describe('#to_h', () => {
+    //
+  });
+
+  describe('#to_hash', () => {
+    //
+  });
+
+  describe('#to_proc', () => {
+    //
+  });
+
+  describe('#to_s', () => {
+    //
+  });
+
+  describe('#trust', () => {
+    //
+  });
+
+  describe('#untaint', () => {
+    //
+  });
+
+  describe('#untrust', () => {
+    //
+  });
+
+  describe('#untrusted?', () => {
+    //
+  });
+
+  describe('#update', () => {
+    //
+  });
+
+  describe('#value?', () => {
+    //
+  });
+
+  describe('#values', () => {
+    //
+  });
+
+  describe('#values_at', () => {
+    //
+  });
+
+  describe('#zip', () => {
+    //
+  });
+
+});
---
+++ test/String-test.js
@@ -0,0 +1,334 @@
+
+var assert = require('assert');
+require('../lib/String');
+
+describe('String', () => {
+  describe('ascii_only?', () => {
+    //
+  });
+
+  describe('#b', () => {
+    //
+  });
+
+  describe('#bytes', () => {
+    //
+  });
+
+  describe('#bytesize', () => {
+    //
+  });
+
+  describe('#byteslice', () => {
+    //
+  });
+
+  describe('#capitalize', () => {
+    //
+  });
+
+  describe('#casecmp', () => {
+    //
+  });
+
+  describe('#center', () => {
+    //
+  });
+
+  describe('#chars', () => {
+    //
+  });
+
+  describe('#chomp', () => {
+    //
+  });
+
+  describe('#chop', () => {
+    //
+  });
+
+  describe('#chr', () => {
+    //
+  });
+
+  describe('#clear', () => {
+    //
+  });
+
+  describe('#codepoints', () => {
+    //
+  });
+
+  describe('#concat', () => {
+    //
+  });
+
+  describe('#count', () => {
+    //
+  });
+
+  describe('#crypt', () => {
+    //
+  });
+
+  describe('#delete', () => {
+    //
+  });
+
+  describe('#downcase', () => {
+    //
+  });
+
+  describe('#dump', () => {
+    //
+  });
+
+  describe('#each_byte', () => {
+    //
+  });
+
+  describe('#each_char', () => {
+    //
+  });
+
+  describe('#each_codepoint', () => {
+    //
+  });
+
+  describe('#each_line', () => {
+    //
+  });
+
+  describe('#empty?', () => {
+    //
+  });
+
+  describe('#encode', () => {
+    //
+  });
+
+  describe('#encoding', () => {
+    //
+  });
+
+  describe('#end_with?', () => {
+    //
+  });
+
+  describe('#eql?', () => {
+    //
+  });
+
+  describe('#force_encoding', () => {
+    //
+  });
+
+  describe('#freeze', () => {
+    //
+  });
+
+  describe('#getbyte', () => {
+    //
+  });
+
+  describe('#gsub', () => {
+    //
+  });
+
+  describe('#hash', () => {
+    //
+  });
+
+  describe('#hex', () => {
+    //
+  });
+
+  describe('#include?', () => {
+    //
+  });
+
+  describe('#index', () => {
+    //
+  });
+
+  describe('#initialize_copy', () => {
+    //
+  });
+
+  describe('#insert', () => {
+    //
+  });
+
+  describe('#inspect', () => {
+    //
+  });
+
+  describe('#intern', () => {
+    //
+  });
+
+  describe('#length', () => {
+    //
+  });
+
+  describe('#lines', () => {
+    //
+  });
+
+  describe('#ljust', () => {
+    //
+  });
+
+  describe('#lstrip', () => {
+    //
+  });
+
+  describe('#match', () => {
+    //
+  });
+
+  describe('#next', () => {
+    //
+  });
+
+  describe('#oct', () => {
+    //
+  });
+
+  describe('#ord', () => {
+    //
+  });
+
+  describe('#partition', () => {
+    //
+  });
+
+  describe('#prepend', () => {
+    //
+  });
+
+  describe('#replace', () => {
+    //
+  });
+
+  describe('#reverse', () => {
+    //
+  });
+
+  describe('#rindex', () => {
+    //
+  });
+
+  describe('#rjust', () => {
+    //
+  });
+
+  describe('#rpartition', () => {
+    //
+  });
+
+  describe('#rstrip', () => {
+    //
+  });
+
+  describe('#scan', () => {
+    //
+  });
+
+  describe('#scrub', () => {
+    //
+  });
+
+  describe('#setbyte', () => {
+    //
+  });
+
+  describe('#size', () => {
+    //
+  });
+
+  describe('#slice', () => {
+    //
+  });
+
+  describe('#split', () => {
+    //
+  });
+
+  describe('#squeeze', () => {
+    //
+  });
+
+  describe('#start_with?', () => {
+    //
+  });
+
+  describe('#strip', () => {
+    //
+  });
+
+  describe('#sub', () => {
+    //
+  });
+
+  describe('#succ', () => {
+    //
+  });
+
+  describe('#sum', () => {
+    //
+  });
+
+  describe('#swapcase', () => {
+    //
+  });
+
+  describe('#to_c', () => {
+    //
+  });
+
+  describe('#to_f', () => {
+    //
+  });
+
+  describe('#to_i', () => {
+    //
+  });
+
+  describe('#to_r', () => {
+    //
+  });
+
+  describe('#to_s', () => {
+    //
+  });
+
+  describe('#to_sym', () => {
+    //
+  });
+
+  describe('#tr', () => {
+    //
+  });
+
+  describe('#tr_s', () => {
+    //
+  });
+
+  describe('#unpack', () => {
+    //
+  });
+
+  describe('#upcase', () => {
+    //
+  });
+
+  describe('#upto', () => {
+    //
+  });
+
+  describe('#valid_encoding?', () => {
+    //
+  });
+
+});
